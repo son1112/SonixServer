d156eaad32335ec4abcc73bc8eab18d0e2d4aaf8
son <son@lincolnix.net>
2014-04-27 07:47:59 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SonixServer::Application.config.secret_key_base = '8ffb241af1130e7c13362da00f144e0aaf7711730985d0ecff0f391ac6e6e05d90dcb2a5195c945e102490bd2a487162693ea952f314a60d1c9d67a73bf26f9f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SonixServer::Application.config.session_store :cookie_store, key: '_SonixServer_session'
